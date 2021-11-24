1f06483fc4c6f437562dff1042673ef6ca6d3890
Simone <startae14@gmail.com>
2021-11-24 12:39:36 +0100

    Class bindings with multiple classes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 let vm = Vue.createApp({
   data() {
     return {
-      isPurple: false
+      isPurple: false,
+      selectedColor: ''
+    }
+  },
+  computed: {
+    circle_classes() {
+      return {
+        purple: this.isPurple
+      };
     }
   }
 }).mount('#app');
